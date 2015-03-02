4f9bd468bf269b04d0e0e9439bd68e6f5931e073
Eliran Levi <eliran@stratoscale.com>
2015-03-02 17:32:23 +0200

    Add activation for offline (for no reason) servers

--- main.py
+++ main.py
@@ -2,14 +2,17 @@
 DB, and sends alerts, by mail, about servers which are not behaving properly"""
 
 
+import os
 import pytz
 import yaml
 import time
+import signal
 import socket
 import logging
 import smtplib
 import datetime
 import traceback
+import subprocess
 from time import sleep
 from email.mime.text import MIMEText
 from rackattack import clientfactory
@@ -24,17 +27,27 @@ SEND_ALERTS_BY_MAIL = True
 SENDER_EMAIL = "eliran@stratoscale.com"
 SMTP_SERVER = 'localhost'
 TIMEZONE = 'Asia/Jerusalem'
+# Cycle length of activation of servers which are offline for no reason
+ACTIVATE_SERVERS_PERIOD = 60 * 60 * 4
 
 
+# State stuff
 state = {'online_for_no_reason': set(),
          'offline_for_no_reason': set()}
+last_time = 0
+msg_so_far = ''
+
+
+def log_msg(msg, level=logging.INFO):
+    """Log the given message and add it to the global `msg_so_far` variable"""
+    logger = logging.getLogger('rackattack_stats')
+    logger.log(level=level, msg=msg)
+    global msg_so_far
+    msg_so_far += msg + '\n'
 
 
 def configure_logger():
     logger = logging.getLogger('rackattack_stats')
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.INFO)
-    logger.addHandler(stream_handler)
     logger.setLevel(logging.INFO)
 
 
@@ -57,13 +70,63 @@ def send_mail(msg):
     s.quit()
 
 
+def pretty_list(lst):
+    """Return a pretty string representation of the given list"""
+    result = '\t* '
+    result += '\n\t* '.join(lst)
+    result += "\n"
+    return result
+
+
+def dump_configuration(configuration):
+    global RACKATTACK_CONFIG_FILENAME
+    yaml.dump(configuration, open(RACKATTACK_CONFIG_FILENAME, 'wb'))
+
+
+def acticvate_servers(servers, cur_time):
+    """Activate the servers in the given list"""
+    # Validate that enough time has passed
+    global last_time
+    if cur_time - last_time < ACTIVATE_SERVERS_PERIOD:
+        return
+    last_time = cur_time
+
+    log_msg('Trying to activate the servers which are offline for no reason.')
+
+    # Turn off all servers first
+    global RACKATTACK_CONFIG_FILENAME
+    configuration = yaml.load(open(RACKATTACK_CONFIG_FILENAME, 'rb'))
+    for server in configuration['HOSTS']:
+        if server['id'] in servers:
+            server['offline'] = True
+    dump_configuration(configuration)
+    # Kill RackAttack
+    try:
+        pid = int(subprocess.check_output(
+            "ps aux | grep 'rackattack\.physical\.main' | awk '{print $2}' ",
+            shell=True))
+    except ValueError:
+        log_msg("Error while activating servers: Cannot find the PID for "
+                "RackAttack for some reason.",
+                level=logging.ERROR)
+        return
+    os.kill(pid, signal.SIGHUP)
+    time.sleep(5)
+
+    # Now turn on the servers
+    for server in configuration['HOSTS']:
+        if server['id'] in servers:
+                server['offline'] = False
+    dump_configuration()
+    # Kill RackAttack
+    os.kill(pid, signal.SIGHUP)
+
+
 def check_no_longer_offline_for_no_reason(offline_for_no_reason,
                                           configured_to_be_online,
                                           actually_online):
     """Return a message string describing nodes no longer offline for no reason.
     """
-    msg = ''
-
     # Servers that were previously "offline for no reason" and are now not,
     # could be in that state either because:
     # *    They were configured to be offline (after last cycle)
@@ -78,19 +141,17 @@ def check_no_longer_offline_for_no_reason(offline_for_no_reason,
                                               if host not in
                                               configured_to_be_online]
     if no_longer_offline_for_no_reason_case_1:
-        msg += 'The following servers were just now configured to be (and ' \
-            'are) offline:\n'
-        msg += str(no_longer_offline_for_no_reason_case_1) + "\n"
+        log_msg('The following servers were just now configured to be, and '
+                'became offline:')
+        log_msg(pretty_list(no_longer_offline_for_no_reason_case_1))
     # The latter case
     no_longer_offline_for_no_reason_case_2 = [host for host in
                                               no_longer_offline_for_no_reason
                                               if host in actually_online]
     if no_longer_offline_for_no_reason_case_2:
-        msg += 'The following servers went back online as configured (after ' \
-            'being offline for some reason):\n'
-        msg += str(no_longer_offline_for_no_reason_case_2) + "\n"
-
-    return msg
+        log_msg('The following servers went back online as configured (after '
+                'being offline for some reason):')
+        log_msg(pretty_list(no_longer_offline_for_no_reason_case_2))
 
 
 def check_no_longer_online_for_no_reason(online_for_no_reason,
@@ -102,8 +163,6 @@ def check_no_longer_online_for_no_reason(online_for_no_reason,
     # "online for no reason" could be in that state either because:
     # *    They were configured to be online (after last cycle)
     # *    or because they went back offline
-
-    msg = ''
     no_longer_online_for_no_reason = set([host for host in
                                           state['online_for_no_reason'] if
                                           host not in online_for_no_reason])
@@ -112,25 +171,39 @@ def check_no_longer_online_for_no_reason(online_for_no_reason,
                                              no_longer_online_for_no_reason if
                                              host in configured_to_be_online]
     if no_longer_online_for_no_reason_case_1:
-        msg += 'The following servers were just now configured to be (and ' \
-            'are) online:\n'
-        msg += str(no_longer_online_for_no_reason_case_1) + "\n"
+        log_msg('The following servers were just now configured to be, and '
+                'became online:')
+        log_msg(pretty_list(no_longer_online_for_no_reason_case_1))
     # The latter case
     no_longer_online_for_no_reason_case_2 = [host for host in
                                              no_longer_online_for_no_reason if
                                              host not in actually_online]
     if no_longer_online_for_no_reason_case_2:
-        msg += 'The following servers went back offline as configured (after' \
-            ' being online for some reason):\n'
-        msg += str(no_longer_online_for_no_reason_case_2) + "\n"
-
-    return msg
+        log_msg('The following servers went back offline as configured (after'
+                ' being online for some reason):')
+        log_msg(pretty_list(no_longer_online_for_no_reason_case_2))
 
 
-def cross_with_nodes_configuration(items):
+def add_whole_state(online_for_no_reason,
+                    offline_for_no_reason,
+                    newly_online_for_no_reason,
+                    newly_offline_for_no_reason):
+    """Add the whole state to the log"""
+    if offline_for_no_reason and \
+            offline_for_no_reason != newly_offline_for_no_reason:
+        log_msg('The entire list of servers which are offline (and are '
+                'not supposed to be) is:')
+        log_msg(pretty_list(offline_for_no_reason))
+    if online_for_no_reason and \
+            online_for_no_reason != newly_online_for_no_reason:
+        log_msg('The entire list of servers which are online (and are '
+                'not supposed to be) is:')
+        log_msg(pretty_list(online_for_no_reason))
+
+
+def check_errornous_servers(items):
     """Add info about nodes that are configured as online but aren't online"""
     # Parse the configuration file
-
     configuration = yaml.load(open(RACKATTACK_CONFIG_FILENAME, 'rb'))
 
     # Get info on errornous servers by comparing the stats to the configuration
@@ -143,44 +216,47 @@ def cross_with_nodes_configuration(items):
                                  host not in actually_online])
 
     # Report errors
-    msg = ''
     newly_offline_for_no_reason = set([host for host in offline_for_no_reason
                                        if host not in
                                        state['offline_for_no_reason']])
     if newly_offline_for_no_reason:
-        msg += 'The following servers were found offline (although ' \
-            'configured to be online):\n'
-        msg += str(newly_offline_for_no_reason) + "\n"
+        log_msg('The following servers were found offline (although '
+                'configured to be online):')
+        log_msg(pretty_list(newly_offline_for_no_reason))
 
     newly_online_for_no_reason = set([host for host in online_for_no_reason if
                                       host not in state['online_for_no_reason']
                                       ])
     if newly_online_for_no_reason:
-        msg += 'The following servers were found online (although configured' \
-            ' to be offline):\n'
-        msg += str(newly_online_for_no_reason) + "\n"
+        log_msg('The following servers were found online (although configured'
+                ' to be offline):')
+        log_msg(pretty_list(newly_online_for_no_reason))
 
-    msg += check_no_longer_offline_for_no_reason(offline_for_no_reason,
-                                                 configured_to_be_online,
-                                                 actually_online)
-    msg += check_no_longer_online_for_no_reason(online_for_no_reason,
-                                                configured_to_be_online,
-                                                actually_online)
+    check_no_longer_offline_for_no_reason(offline_for_no_reason,
+                                          configured_to_be_online,
+                                          actually_online)
+    check_no_longer_online_for_no_reason(online_for_no_reason,
+                                         configured_to_be_online,
+                                         actually_online)
 
     # Update state
     state['offline_for_no_reason'] = offline_for_no_reason
     state['online_for_no_reason'] = online_for_no_reason
 
-    if msg:
-        logger = logging.getLogger('rackattack_stats')
-        logger.info(msg)
-        global SEND_ALERTS_BY_MAIL
-        if SEND_ALERTS_BY_MAIL:
-            send_mail(msg)
+    # Add the whole state, for convenience (this might be different than the
+    # previously-sent lists, since this is the state, and the lists sent so far
+    # were diffs.
+    add_whole_state(online_for_no_reason,
+                    offline_for_no_reason,
+                    newly_online_for_no_reason,
+                    newly_offline_for_no_reason)
 
 
 def fetch_stats(rackattack_client, db):
     """Fetch RackAttack stats, add them to db and alert on errors"""
+    logger = logging.getLogger('rackattack_stats')
+    logger.debug('Working...')
+
     unixtime = int(time.time() * 1000)
     cur_time = pytz.timezone(TIMEZONE).localize(datetime.datetime.now())
     # Get stats from RackAttack
@@ -188,7 +264,6 @@ def fetch_stats(rackattack_client, db):
 
     # Insert stats to the DB
     for collection_name, items in stats.iteritems():
-        logger = logging.getLogger('rackattack_stats')
         logger.debug('Inserting {} records to to collection "{}"'
                      .format(len(items), collection_name))
 
@@ -207,21 +282,34 @@ def fetch_stats(rackattack_client, db):
                       id=id)
 
     # Use a special index that already counts the states (typos are in
-    # rackattack)
+    # rackattack). This is quite ugly, but we haven't managed to craete a
+    # query in Kibana which does an average on the count of states (2
+    # aggregations)
     states = ('INAUGURATION_DONE', 'CHECKED_IN',
               'SLOW_RECLAIMATION_IN_PROGRESS', 'INAUGURATION_LABEL_PROVIDED',
               'QUICK_RECLAIMATION_IN_PROGRESS')
-    for state in states:
-        record = {'state': state,
+    for _state in states:
+        record = {'state': _state,
                   'states_count': len([host for host in stats['hosts'] if
-                                       host['state'] == state]),
+                                       host['state'] == _state]),
                   'timestamp': cur_time,
                   '_timestamp': cur_time}
         id = "%d%03d" % (unixtime, index)
-        db.create(index='states', doc_type='state_count', body=record, )
+        db.create(index='states', doc_type='state_count', body=record)
 
     # Alert about servers that died and stuff
-    cross_with_nodes_configuration(stats['hosts'])
+    check_errornous_servers(stats['hosts'])
+
+    # Activate servers which are offline for no reason
+    global state
+    acticvate_servers(servers=state['offline_for_no_reason'],
+                      cur_time=unixtime)
+
+    global msg_so_far
+    if msg_so_far:
+        global SEND_ALERTS_BY_MAIL
+        if SEND_ALERTS_BY_MAIL:
+            send_mail(msg_so_far)
 
 
 def main():
