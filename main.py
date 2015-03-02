3169df6ab1a1e8b8805c5783ffe08e8d138b18c1
Eliran Levi <eliran@stratoscale.com>
2015-03-02 18:17:52 +0200

    Disable activation in the meantime

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-"""This monitors RackAttack's nodes periodically, inserts them to a dedicated
-DB, and sends alerts, by mail, about servers which are not behaving properly"""
+"""This script does several things:
+* Monitors RackAttack's nodes periodically and inserts them to a dedicated
+  ElasticSearch DB
+* Sends alerts, by mail, about servers which are not behaving properly
+* Monitors RackAttack's Allocation failure log records, and insert them info
+  a dedicated ElasticSearch DB.
+"""
 
 
 import os
 import pytz
 import yaml
 import time
+import errno
 import signal
 import socket
 import logging
@@ -14,26 +20,37 @@ import datetime
 import traceback
 import subprocess
 from time import sleep
+from StringIO import StringIO
 from email.mime.text import MIMEText
 from rackattack import clientfactory
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, NotFoundError
+# from rackattack.common.hoststatemachine import ALLOCATION_LOG_FILENAME
+ALLOCATION_LOG_FILENAME = "/var/log/rackattack-allocation-failures.json"
 
-
+# Interesting configuration
 SAMPLE_INTERVAL_NR_SECONDS = 60
 EMAIL_SUBSCRIBERS = ("eliran@stratoscale.com",)
-RACKATTACK_CONFIG_FILENAME = '/etc/rackattack.physical.rack.yaml'
-DB_NAME = 'rackattack_stats'
 SEND_ALERTS_BY_MAIL = True
+
+
+# Less interesting configuration
+RAP_CONFIGURATION_FILENAME = 'rackattack.physical.rack.yaml'
 SENDER_EMAIL = "eliran@stratoscale.com"
 SMTP_SERVER = 'localhost'
 TIMEZONE = 'Asia/Jerusalem'
+RAP_PASSWORD = 'strato'
 # Cycle length of activation of servers which are offline for no reason
+UPDATE_ALLOCATION_FAILURE_DB_PERIOD = 60 * 10
+RAP_CONFIGURATION_FILEPATH = '/etc/{}'.format(RAP_CONFIGURATION_FILENAME)
+TEMP_DIR_PATH = '/tmp'
+# This currently is not doint anything:
 ACTIVATE_SERVERS_PERIOD = 60 * 60 * 4
 
 
 # State stuff
 state = {'online_for_no_reason': set(),
-         'offline_for_no_reason': set()}
+         'offline_for_no_reason': set(),
+         'allocation_failures': []}
 last_time = 0
 msg_so_far = ''
 
@@ -188,23 +205,35 @@ def add_whole_state(online_for_no_reason,
                     offline_for_no_reason,
                     newly_online_for_no_reason,
                     newly_offline_for_no_reason):
-    """Add the whole state to the log"""
-    if offline_for_no_reason and \
-            offline_for_no_reason != newly_offline_for_no_reason:
-        log_msg('The entire list of servers which are offline (and are '
-                'not supposed to be) is:')
-        log_msg(pretty_list(offline_for_no_reason))
-    if online_for_no_reason and \
-            online_for_no_reason != newly_online_for_no_reason:
-        log_msg('The entire list of servers which are online (and are '
-                'not supposed to be) is:')
-        log_msg(pretty_list(online_for_no_reason))
+    # Only write stuff in case there are new errors (since this is sent by 
+    # mail
+    global msg_so_far
+    if msg_so_far:
+        if offline_for_no_reason:
+            servers_list = ' '.join(offline_for_no_reason)
+            cmd = "python ~/return_servers_to_rack.py {}".format(servers_list)
+            log_msg("\nRun the following command on rackattack-provider.dc1."
+                    "strato: {}".format(cmd))
 
 
-def check_errornous_servers(items):
+        if offline_for_no_reason and \
+                offline_for_no_reason != newly_offline_for_no_reason:
+            log_msg('The entire list of servers which are offline (and are '
+                    'not supposed to be) is:')
+            log_msg(pretty_list(offline_for_no_reason))
+
+
+        if online_for_no_reason and \
+                online_for_no_reason != newly_online_for_no_reason:
+            log_msg('The entire list of servers which are online (and are '
+                    'not supposed to be) is:')
+            log_msg(pretty_list(online_for_no_reason))
+
+
+def check_errornous_servers(items, configuration):
     """Add info about nodes that are configured as online but aren't online"""
     # Parse the configuration file
-    configuration = yaml.load(open(RACKATTACK_CONFIG_FILENAME, 'rb'))
+    configuration = yaml.load(StringIO(configuration))
 
     # Get info on errornous servers by comparing the stats to the configuration
     configured_to_be_online = \
@@ -252,13 +281,44 @@ def check_errornous_servers(items):
                     newly_offline_for_no_reason)
 
 
-def fetch_stats(rackattack_client, db):
+def fetch_from_rap(filepath):
+    # Transfer the file to a local dir
+    global TEMP_DIR_PATH
+    dest_filepath = os.path.join(TEMP_DIR_PATH, os.path.basename(filepath))
+    cmd = 'sshpass -p "{}" scp root@{}:{} {}' \
+          .format(RAP_PASSWORD,
+                  os.environ['RAP_ADDR'],
+                  filepath,
+                  dest_filepath)
+    subprocess.check_call(cmd, shell=True)
+
+    # Read the local file
+    content = ''
+    with open(dest_filepath, 'rb') as f:
+        content = f.read()
+    return content
+
+
+def datetime_from_timestamp(timestamp):
+    global TIMEZONE
+    datetime_now = datetime.datetime.fromtimestamp(timestamp)
+    datetime_now = pytz.timezone(TIMEZONE).localize(datetime_now)
+    return datetime_now
+
+
+def fetch_nodes_stats(rackattack_client, db, timestamp):
     """Fetch RackAttack stats, add them to db and alert on errors"""
     logger = logging.getLogger('rackattack_stats')
-    logger.debug('Working...')
 
-    unixtime = int(time.time() * 1000)
-    cur_time = pytz.timezone(TIMEZONE).localize(datetime.datetime.now())
+    global msg_so_far
+    msg_so_far = ''
+
+    unixtime = int(timestamp * 1000)
+    datetime_now = datetime_from_timestamp(timestamp)
+
+    logger.debug('Fetching configuration from RAP...')
+    configuration = fetch_from_rap(RAP_CONFIGURATION_FILEPATH)
+
     # Get stats from RackAttack
     stats = rackattack_client.call('admin__queryStatus')
 
@@ -271,8 +331,8 @@ def fetch_stats(rackattack_client, db):
         for index, item in enumerate(items):
             # generate a unique id (id field is mandatory :/ )
             id = "%d%03d" % (unixtime, index)
-            item['timestamp'] = cur_time
-            item['_timestamp'] = cur_time
+            item['timestamp'] = datetime_now
+            item['_timestamp'] = datetime_now
 
             collection_name_to_index = {'hosts': 'hosts_stats',
                                         'allocations': 'allocations'}
@@ -292,28 +352,68 @@ def fetch_stats(rackattack_client, db):
         record = {'state': _state,
                   'states_count': len([host for host in stats['hosts'] if
                                        host['state'] == _state]),
-                  'timestamp': cur_time,
-                  '_timestamp': cur_time}
+                  'timestamp': datetime_now,
+                  '_timestamp': datetime_now}
         id = "%d%03d" % (unixtime, index)
         db.create(index='states', doc_type='state_count', body=record)
 
     # Alert about servers that died and stuff
-    check_errornous_servers(stats['hosts'])
+    check_errornous_servers(stats['hosts'], configuration)
 
-    # Activate servers which are offline for no reason
-    global state
-    acticvate_servers(servers=state['offline_for_no_reason'],
-                      cur_time=unixtime)
-
-    global msg_so_far
     if msg_so_far:
         global SEND_ALERTS_BY_MAIL
         if SEND_ALERTS_BY_MAIL:
             send_mail(msg_so_far)
 
 
+def fetch_allocation_failures(db):
+    log = fetch_from_rap(ALLOCATION_LOG_FILENAME)
+    log = yaml.load(StringIO(log))
+
+    index_name = 'allocation_failures'
+    doc_type = 'allocation_failure'
+
+    # Validate that the state is contained in the log (from the beginning)
+    if len(log) < len(state['allocation_failures']):
+        state['allocation_failures'] = []
+    else:
+        for idx, state_log_record in enumerate(state['allocation_failures']):
+            if state_log_record != log[idx]:
+                # Reset the state
+                state['allocation_failures'] = []
+                break
+
+    # Insert the new records in the log (which are not in the state)
+    new_records = log[len(state['allocation_failures']):]
+    for timestamp, host_id in new_records:
+        record_datetime = datetime_from_timestamp(timestamp)
+        # Validate first that such record does not exist (this might occur
+        # if the script started executing, with the same DB, after crashing).
+        # I would use the upsert method of elasticsearch, but there's no such
+        # method in elasticserachpy as far as i know.
+        # Convert record timestamp to ElasticSearch's format
+        record_datetime_str = str(record_datetime).replace(' ', 'T')
+        query = "host_id:\"{}\" AND timestamp:\"{}\"". \
+            format(host_id, record_datetime_str)
+        try:
+            result = db.search(index=index_name, doc_type=doc_type, q=query)
+            if result['hits']['total'] > 0:
+                # Skip this log record since it already exists in the DB
+                print '\n\n\t\tSkipping\n\n'
+                continue
+        except NotFoundError:
+            pass
+
+        # Insert the record at last
+        record = {"_timestamp": record_datetime,
+                  "timestamp": record_datetime,
+                  "host_id": host_id}
+        db.create(index=index_name, doc_type=doc_type, body=record)
+
+
 def main():
     configure_logger()
+    logger = logging.getLogger('rackattack_stats')
     # Connect to RackAttack
     client = clientfactory.factory()
     # Connect to ElasticSearch
@@ -322,11 +422,22 @@ def main():
     # Fetch stats forever
     while True:
         try:
-            fetch_stats(client, db)
+            fetch_nodes_stats(client, db, time.time())
         except KeyboardInterrupt:
             return
+        except socket.error, e:
+            logger.error(traceback.format_exc())
+            if isinstance(e.args, tuple):
+                if e[0] in (errno.EPIPE, errno.EBADF):
+                    logger.error("Detected disconnection (error {}), trying "
+                                 "to reconnect...".format(e[0]))
+                    client.close()
+                    client = clientfactory.factory()
+                else:
+                    logger.error("Unknown socket error: {}".format(str(e)))
+            else:
+                logger.error("Unknown socket error: {}".format(str(e)))
         except Exception:
-            logger = logging.getLogger('rackattack_stats')
             logger.error(traceback.format_exc())
         finally:
             sleep(SAMPLE_INTERVAL_NR_SECONDS)
