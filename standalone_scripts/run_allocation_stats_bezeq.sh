22967146e647a7991dedefcab2f0553d1ae56e0d
Eliran Levi <eliran@stratoscale.com>
2015-07-09 16:18:20 +0300

    Add a standalone allocation-stats script for bezeq

---
+++ standalone_scripts/run_allocation_stats_bezeq.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+RAP_URI='rackattack-provider.dc1' RACKATTACK_PROVIDER=tcp://${RAP_URI}:1014@@amqp://guest:guest@${RAP_URI}:1013/%2F@@http://${RAP_URI}:1016 UPSETO_JOIN_PYTHON_NAMESPACES=yes PYTHONPATH=py python ~/work/rackattack-stats/py/rackattack/stats/main_allocation_stats.py
