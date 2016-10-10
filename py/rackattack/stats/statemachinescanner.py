ffdc26d356b5b67089fcb588df412e56c0edce47
Eliran Levi <eliran@stratoscale.com>
2016-10-10 09:06:16 +0000

    Compile the regex pattern before searching

--- py/rackattack/stats/statemachinescanner.py
+++ py/rackattack/stats/statemachinescanner.py
@@ -6,13 +6,13 @@ class StateMachineScanner:
     STATE_OUTSIDE_RESULT = 2
 
     def __init__(self, start_pattern, stop_pattern):
-        self._start_pattern = start_pattern
-        self._stop_pattern = stop_pattern
+        self._start_pattern = re.compile(start_pattern)
+        self._stop_pattern = re.compile(stop_pattern)
         self._patterns = []
         self._current_result = None
 
     def add_pattern(self, pattern):
-        self._patterns.append(pattern)
+        self._patterns.append(re.compile(pattern))
 
     def scan(self, content):
         self._initialize_scan()
@@ -27,18 +27,18 @@ class StateMachineScanner:
     def _scan_line(self, line):
         result = None
         if self._state == self.STATE_OUTSIDE_RESULT:
-            results = re.search(self._start_pattern, line)
+            results = self._start_pattern.match(line)
             if results is not None:
                 results = results.groups()
                 self._current_result = dict(start=results)
                 self._state = self.STATE_INSIDE_RESULT
         elif self._state == self.STATE_INSIDE_RESULT:
             for pattern in self._patterns:
-                results = re.search(pattern, line)
+                results = pattern.match(line)
                 if results is not None:
                     results = results.groups()
                     self._current_result.setdefault("matches", []).append(results)
-            results = re.search(self._stop_pattern, line)
+            results = self._stop_pattern.match(line)
             if results is not None:
                 results = results.groups()
                 self._state = self.STATE_OUTSIDE_RESULT
